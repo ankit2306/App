76f9b3f76f66854cdc06541e1a1c0b0333cb73dd
ankit2306 <ankit.a.ankit@capgemini.com>
2019-09-27 17:39:03 +0530

    gallery

--- App-UI/src/app/app.module.ts
+++ App-UI/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FormsModule } from '@angular/forms';
 import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
+import { NgxGalleryModule } from 'ngx-gallery';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -42,6 +43,7 @@ export function tokenGetter() {
       FormsModule,
       BsDropdownModule.forRoot(),
       TabsModule.forRoot(),
+      NgxGalleryModule,
       RouterModule.forRoot(appRoutes),
       JwtModule.forRoot({
          config: {
